1070d4a561e88e2519d6cd2f28d49bad9208189e
Nawaf-sds <nwaf.o.th@gmail.com>
2026-05-14 15:34:43 +0300

    Update MongoDB.js

--- Back-end/MongoDB/MongoDB.js
+++ Back-end/MongoDB/MongoDB.js
@@ -1,22 +1,4 @@
 // Back-end/MongoDB/MongoDB.js
-
-// const mongoose = require('mongoose');
-// const config = require('../Config/Config'); // ✅ استدعاء config.js
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(config.mongoURI);
-//     console.log('✅ MongoDB Connected');
-//   } catch (error) {
-//     console.error('❌ MongoDB connection failed:', error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB; 
-
-
-
 const mongoose = require('mongoose');
 const config = require('../Config/Config'); 
 
