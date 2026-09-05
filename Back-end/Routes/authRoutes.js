e2623e2cfba53e068d7c88c4a296575004e3849b
Nawaf-sds <nwaf.o.th@gmail.com>
2026-09-05 13:47:04 +0300

    Update authRoutes.js

--- Back-end/Routes/authRoutes.js
+++ Back-end/Routes/authRoutes.js
@@ -1,20 +1,8 @@
-// // في ملف Routes/authRoutes.js
-// const express = require('express');
-// const router = express.Router();
-// const { registerUser, loginUser, verifyUser } = require('../controllers/authController');
-// const { protect } = require('../middleware/authMiddleware');
 
-// router.post('/register', registerUser);
-// router.post('/login', loginUser);
-// router.get('/verify', protect, verifyUser); // ✅ مسار جديد
-
-
-// module.exports = router;
 
 
 const express = require('express');
 const router = express.Router();
-// 🛠️ التعديل هنا: أضفنا googleLogin داخل الأقواس لتتمكن من استخدامها
 const { 
   registerUser, 
   loginUser, 
